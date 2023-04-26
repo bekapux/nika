ac820b624b3335c205c820be5268308fc539d6f3
Beka Pukhashvili <beka.pukhashvili@dataart.com>
2023-04-26 14:10:27 +0400

    minor update

--- download-images.js
+++ download-images.js
@@ -5,7 +5,7 @@ const sharp = require('sharp');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({ width: 200, height: 300 });
+  await page.setViewport({ width: 1240, height: 1656 });
 
   const url = 'http://188.129.139.228:8080/?fbclid=IwAR2Rca9fH0LLkdKzfq6Skow3pyOx0ucKAeuzvTnXQ6MMBr4H2IymKN6Q7Gk';
   await page.goto(url, { waitUntil: 'networkidle2' });
@@ -16,7 +16,7 @@ const sharp = require('sharp');
     const characterDiv = characterDivs[i];
     const imageBuffer = await characterDiv.screenshot({ omitBackground: true });
 
-    fs.writeFileSync(`chars/character-${i + 1}.png`, imageBuffer);
+    fs.writeFileSync(`character-${i + 1}.png`, imageBuffer);
   }
 
   await browser.close();
